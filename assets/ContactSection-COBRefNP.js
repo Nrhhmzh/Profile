4cde9afcb06fdcc8f55d473b221ab31af33b70d2
Nrhhmzh <69445191+Nrhhmzh@users.noreply.github.com>
2025-02-11 14:54:10 +0800

    adding dist

---
+++ assets/ContactSection-COBRefNP.js
@@ -0,0 +1,1 @@
+import{o as s,c as r,a}from"./index-Ccr0sXBH.js";const l=(e,t)=>{const o=e.__vccOpts||e;for(const[c,n]of t)o[c]=n;return o},x={},_={class:"text-white mt-20",id:"contact"};function f(e,t){return s(),r("section",_,t[0]||(t[0]=[a("h2",{class:"text-4xl font-bold text-white text-left mb-4 px-4 xl:pl-16"},"Let's Connect",-1)]))}const d=l(x,[["render",f]]);export{d as default};
